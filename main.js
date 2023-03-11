30995030711a5825ee143ee27bc47601cfa9e89d
Ale <97320635+marilesmartineza@users.noreply.github.com>
2023-03-11 15:09:30 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+   Hat=loadImage("https://i.postimg.cc/pdLdZnMh/sombrero.png");
+
+}
+
+ function setup() {
+    canvas=createCanvas(300,300);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modeLoaded);
+    poseNet.on("pose", gotPoses);
+
+   }
+
+   function modeLoaded(){
+      console.log("poseNet está inicializado");
+   }
+
+   function gotPoses(results){
+      if(results.length>0){
+         console.log(results),
+         noseX=results[0].pose.nose.x -50;
+         noseY=results[0].pose.nose.y -150;
+         console.log("nose x = "+ results[0].pose.nose.x);
+         console.log("nose y = "+ results[0].pose.nose.y);
+      }
+   }
+
+   function draw() {
+      image(video,0,0,300,300);
+      fill(255.0,0);
+      stroke(103,21,21);
+      //circle(noseX,noseY,30);
+      image(Hat,noseX,noseY,100,90);
+
+   }
+
+   function take_snapshot() {
+       save("Foto.jpg");
+   }
+
+
+ 
